262c046e3542b31ab08086a67ff0fb03e95585ef
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2025-11-06 16:25:50 +0100

    feat(extract): implement parse_toc

--- extract/books.py
+++ extract/books.py
@@ -5,15 +5,50 @@ class Book:
     def __init__(self, name, *, href):
         self.name = name
         self.href = href
+    
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return f'<Book name="{self.name}">'
 
-def parse_toc(tree: ElementTree):
+def is_valid_title(text: str) -> bool:
+    """
+    Checks if it is a valid bible book title.
+    """
+
+    text = text.lower()
+
+    for s in ('biblia', 'strona', 'starego', 'nowego', 'publikacji'):
+        if s in text:
+            return False
+    
+    return True
+
+def parse_toc(tree: ElementTree) -> list[Book]:
     """
     Extracts all books in the table of contents.
     """
+
     root = tree.getroot()
-    # <navPoint id="c10_Biblia_Wujka__1923__Pierwsza_Ksiega_Samuela" playOrder="12">
-    # 						<navLabel><text>Królewskie pierwsze (Samuelowe I)</text></navLabel>
-    # 						<content src="c10_Biblia_Wujka__1923__Pierwsza_Ksiega_Samuela.xhtml" />
-    # 				</navPoint>
+    namespace = {'ncx': 'http://www.daisy.org/z3986/2005/ncx/'}
     
-    return []
+    nav_points = root.findall('.//ncx:navPoint', namespace)
+    
+    result = []
+    
+    for chapter in nav_points:
+        title = chapter.find('ncx:navLabel/ncx:text', namespace)
+        content = chapter.find('ncx:content', namespace)
+
+        # ignore invalid chapters
+        if title.text.isnumeric() or content is None or not is_valid_title(title.text):
+            continue
+        
+        href = content.attrib['src']
+
+        # prevent duplicates
+        if href not in map(lambda b: b.href, result):
+            result.append(Book(title.text, href=href))
+    
+    return result
--- extract/books_test.py
+++ extract/books_test.py
@@ -1,11 +1,20 @@
 import unittest
 import xml.etree.ElementTree as ET
 
-from extract.books import parse_toc
+from extract.books import parse_toc, is_valid_title
 
 class TestBooks(unittest.TestCase):
+    def test_is_valid_title(self):
+        self.assertFalse(is_valid_title('Biblia, to jest księgi Starego i Nowego Testamentu'))
+        self.assertFalse(is_valid_title('Starego Testamentu'))
+        self.assertFalse(is_valid_title('Nowego Testamentu'))
+        self.assertTrue(is_valid_title('Genesis, to jest pierwsze'))
+        self.assertTrue(is_valid_title('Objawienie św. Jana'))
+
+
     def test_parse_toc(self):
         tree = ET.parse('tests/toc.ncx')
         books = parse_toc(tree)
 
         self.assertEqual(66, len(books))
+        self.assertIn('Genesis', books[0].name)
